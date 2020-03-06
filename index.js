8d230145207bf32b5b009b49de10a7fd54ebc952
Iván Martínez <ivanmartinez33@outlook.com>
2020-03-06 10:54:48 -0600

    index.js agregado

---
+++ index.js
@@ -0,0 +1,35 @@
+// Importar dependencias
+const mongoose = require('mongoose')
+
+// Importar modelos
+const AnswerModel = require('./models/Answer')
+const CommentModel = require('./models/Comment')
+const ExamModel = require('./models/Exam')
+const ExamSessionModel = require('./models/ExamSession')
+const FieldModel = require('./models/Field')
+const QuestionModel = require('./models/Question')
+const SessionQuestionModel = require('./models/SessionQuestion')
+const UniversityModel = require('./models/University')
+const UserModel = require('./models/User')
+
+/**
+ * Conectarse a la base de datos y obtener los modelos
+ * @param {String} database_url
+ */
+async function connect(database_url) {
+  // Conectarse a la base de datos
+  await mongoose.connect(config.databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  // Regresar los modelos
+  return {
+    AnswerModel,
+    CommentModel,
+    ExamModel,
+    ExamSessionModel,
+    FieldModel,
+    QuestionModel,
+    SessionQuestionModel,
+    UniversityModel,
+    UserModel
+  }
+}
