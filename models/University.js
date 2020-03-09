25cb49e5e2fade18de57936f106bb866b4ecd460
Iván Martínez <ivanmartinez33@outlook.com>
2020-03-09 09:17:58 -0600

    Funciones nuevas agregadas al campo University

--- models/University.js
+++ models/University.js
@@ -16,11 +16,38 @@ const UniversitySchema = new mongoose.Schema({
     type: [{ type: mongoose.Types.ObjectId, ref: config.schemasNames.field }],
     required: true,
     default: []
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: true
   }
 }, { timestamps: true })
 
 // Configurar uniqueValidator para el Schema
 UniversitySchema.plugin(UniversitySchema)
+
+// Agregar una área a una universidad
+UniversitySchema.statics.addField = async function (universityId, fieldId) {
+  // Obtener unviersidad y sus áreas
+  const university = await University.findById(universityId)
+  const fields = university.fields
+
+  // Asegurarse de que no existe la área
+  if (university.fields.indexOf(fieldId) == -1) {
+    // Actualizar campos 'fields' de la universidad
+    return University.findOneAndUpdate({ _id: universityId }, {
+      fields: fields.concat([fieldId])
+    })
+  } else {
+    // TODO: Lanzar un error
+  }
+}
+
+// Obtener todas las universidades
+UniversitySchema.statics.getUniversities = async function () {
+  return University.find({ isActive: true })
+}
 
 // Exportar modelo
 const University = mongoose.model(config.schemasNames.university, UniversitySchema)
