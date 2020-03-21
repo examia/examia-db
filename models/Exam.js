0dad7e19a6e99cea6824d54c4bf294c3678d89b7
Iván Martínez <ivanmartinez33@outlook.com>
2020-03-21 17:31:44 -0600

    Se agregó el campo examId al modelo ExamSection

--- models/Exam.js
+++ models/Exam.js
@@ -1,7 +1,5 @@
 // Importar dependencias
 const mongoose = require('mongoose');
-const errorMessages = require('../errorHandling/errorMessages');
-const errors = require('../errorHandling/errors');
 
 // Importar configuración
 const config = require('./config');
@@ -13,7 +11,6 @@ const config = require('./config');
  * @param {Object} - Objeto con todas las propiedades
  * @property {mongoose.Types.ObjectId} fieldId - Id del área del examen
  * @property {number} year - Año del examen
- * @property {Array.<mongoose.Types.ObjectId>} questions - Preguntas del examen
  */
 const ExamSchema = new mongoose.Schema({
   fieldId: {
@@ -24,37 +21,10 @@ const ExamSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
-  sections: {
-    type: [{ type: mongoose.Types.ObjectId, ref: config.schemasNames.examSection }],
-    required: true,
-    default: [],
-  },
 }, { timestamps: true });
 
 /** @mixin */
 ExamSchema.statics;
-
-/**
- * Agrega una sección a un examen
- * @param {mongoose.Types.ObjectId} examId - Id del examen
- * @param {mongoose.Types.ObjectId} sectionId  - Id de la sección del examen
- */
-ExamSchema.statics.addSection = async (examId, sectionId) => {
-  // Obtener examen y sus secciones
-  const examSection = await ExamSchema.findById(examId);
-  const { sections } = examSection;
-
-  // Asegurarse de que la sección no está en el examen
-  if (examSection.sections.indexOf(sectionId) === -1) {
-    // Actualizar campo 'sections' del examen
-    return ExamSchema.findOneAndUpdate({ _id: examId }, {
-      sections: sections.concat([sectionId]),
-    }, { new: true });
-  }
-
-  // Lanzar un error
-  throw new errors.DuplicatedId(errorMessages.exam.duplicatedSectionId);
-};
 
 /**
  * Obtiene los exámenes de una área
--- models/ExamSection.js
+++ models/ExamSection.js
@@ -9,18 +9,17 @@ const config = require('./config');
  * @class ExamSection
  * @mixes {ExamSectionSchema.statics}
  * @param {Object} - Objeto con todas las propiedades
+ * @property {mongoose.Types.ObjectId} examId - Id del examen
  * @property {number} position - Posición de la sección den el examen
- * @property {Array.<mongoose.Types.ObjectId>} questions - Preguntas de la sección
  */
 const ExamSectionSchema = new mongoose.Schema({
+  examId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
   position: {
     type: Number,
     required: true,
-  },
-  questions: {
-    type: [{ type: mongoose.Types.ObjectId, ref: config.schemasNames.question }],
-    required: true,
-    default: [],
   },
 }, { timestamps: true });
 
@@ -28,26 +27,10 @@ const ExamSectionSchema = new mongoose.Schema({
 ExamSectionSchema.statics;
 
 /**
- * Agrega una pregunta a un examen
+ * Busca las secciones de un examen
  * @param {mongoose.Types.ObjectId} examId - Id del examen
- * @param {mongoose.Types.ObjectId} questionId  - Id de la pregunta
  */
-ExamSectionSchema.statics.addQuestion = async (examId, questionId) => {
-  // Obtener examen y sus preguntas
-  const examSection = await ExamSection.findById(examId);
-  const { questions } = examSection;
-
-  // Asegurarse de que la pregunta no está en el examen
-  if (examSection.questions.indexOf(questionId) === -1) {
-    // Actualizar campo 'questions' del examen
-    return ExamSection.findOneAndUpdate({ _id: examId }, {
-      questions: questions.concat([questionId]),
-    }, { new: true });
-  }
-
-  // TODO: Modificar el manejo de errores
-  throw new Error('La pregunta ya está en el examen');
-};
+ExamSectionSchema.statics.findByExamId = (examId) => ExamSection.find({ examId });
 
 // Exportar modelo
 const ExamSection = mongoose.model(config.schemasNames.examSection, ExamSectionSchema);
